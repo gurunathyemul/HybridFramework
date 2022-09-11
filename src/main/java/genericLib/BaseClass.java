53706b6ed187c532877497cf84efba8a240c91d3
Your Name <you@example.com>
2022-09-12 00:20:06 +0530

    HybridFramework(MAVEN Project)

--- src/main/java/genericLib/BaseClass.java
+++ src/main/java/genericLib/BaseClass.java
@@ -5,9 +5,11 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -16,11 +18,17 @@ public class BaseClass {
 	public WebDriver driver;
 	public Propertyfille pdata=new Propertyfille();
 	public WebDriverUtilies driverutilies=new WebDriverUtilies();
-	
+	@Parameters(value = {"browserName"})
 	@BeforeMethod
-	public void openAPP() throws IOException {
+	public void openAPP(String browserName) throws IOException {
 		WebDriverManager.chromedriver().setup();
+		WebDriverManager.edgedriver().setup();
+		if(browserName.equals("chrome")) {
 		driver=new ChromeDriver();
+		}
+		else {
+			driver=new EdgeDriver();
+		}
 		driver.manage().window().maximize();
 		driver.get(pdata.getPropertyfile("url"));
 		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
