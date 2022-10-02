61d09c655f98d1ba3f419ef1b99831e42affeaa4
gurunathyemul <108144823+gurunathyemul@users.noreply.github.com>
2022-10-02 11:59:34 +0530

    Update BaseClass.java

--- src/main/java/genericLib/BaseClass.java
+++ src/main/java/genericLib/BaseClass.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
